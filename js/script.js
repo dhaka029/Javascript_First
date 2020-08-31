59654ecf7f58af43449e0c80b97d08ac20c006fe
Md. Arifur Rahman <dhaka029@gmail.com>
2020-09-01 01:41:08 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function textChange(){
+	document.getElementById("p").innerHTML="First Content";
+	document.getElementById("p1").innerHTML="Try to Make some variation";
+	document.getElementById("p2").innerHTML="Dummy Content";
+	document.getElementById("p3").innerHTML="Hi Guyes How R U ?";
+	document.getElementById("p4").innerHTML="Hello SSB-276";
+	document.getElementById("p5").innerHTML="We Will Make some difference";
+}
